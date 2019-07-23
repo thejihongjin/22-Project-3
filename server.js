176b28e3d068016d8655a61b3a87b1abe61642a2
Anh Dao <addao823@gmail.com>
2019-07-22 20:28:00 -0700

    Initial set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path");
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+const db = require("./model")
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//mongoose connection, need to add database name
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', { useNewUrlParser: true });
+
+//Routes
+
+/// Default route for React
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+}) 
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on PORT ${PORT}!`)
+})
+
