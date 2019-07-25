fb7ee00336c71b81ffd9eba8417acf2128464827
tjlyttles <tjlyttles84@gmail.com>
2019-07-24 18:30:27 -0700

    added user and create pages; modified app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/Loading";
+const User = React.lazy(() => import("./pages/User"));
+const CreateEvent = React.lazy(() => import("./pages/CreateEvent"));
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path="/user" component={User} />
+            <Route exact path="/create" component={CreateEvent} />
+            <Route render={() => <h1>404 Page not found.</h1>} />
+          </Switch>
+        </React.Suspense>
+      </Router>
+    );
+  }
 }
 
 export default App;
