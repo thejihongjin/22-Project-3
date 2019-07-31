500dae2b5ae393447d99bc9a400c21e3bf2e97d9
Anh Dao <addao823@gmail.com>
2019-07-31 15:42:10 -0700

    Add userContext, change router object, add util history

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,19 +3,35 @@ import User from "../pages/User";
 import { Container, Row, Col } from "react-bootstrap";
 import { Form, Button } from "react-bootstrap";
 import API from "../utils/API";
+import { useUserContext} from "../utils/userContext"
+import history from '../utils/history';
 
 function Register(props) {
   const [newUserName, setNewUserName] = useState("");
   const [newEmail, setNewEmail] = useState("");
   const [newPassWord, setNewPassWord] = useState("");
   const [passWordVer, setPassWordVer] = useState("");
+  const [state, dispatch] = useUserContext()
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
     if (newPassWord !== passWordVer) {
       alert("passwords must match");
+    } else {
+      API.registerUser(
+        {
+          username: newUserName,
+          displayname: newUserName,
+          email: newEmail,
+          password: passWordVer
+        }).then(
+          returnUser => { dispatch({ type:"registerNewUser", user:returnUser.data}) }
+        ).then(
+          history.push('/user')
+        )
     }
-    API.registerUser(
+
+    /*API.registerUser(
       {
         username: newUserName,
         displayname: newUserName,
@@ -23,8 +39,8 @@ function Register(props) {
         password: passWordVer
       },
       result => console.log(result)
-    );
-  };
+    );*/
+  }
   return (
     <Container>
       <Form onSubmit={handleSubmit}>
