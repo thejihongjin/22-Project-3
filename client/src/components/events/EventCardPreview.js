a62b471f529d24afdfbad3e45af47d01ce032dea
tjlyttles <tjlyttles84@gmail.com>
2019-08-09 10:58:56 -0700

    save before merge

--- client/src/components/events/EventCardPreview.js
+++ client/src/components/events/EventCardPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Alert from "react-bootstrap/Alert";
@@ -8,77 +8,90 @@ import JoinButton from "../buttons/JoinButton";
 import LeaveButton from "../buttons/LeaveButton";
 
 const EventCardPreview = props => {
-    const user = props.user;
-    const event = props.event;
+  const user = props.user;
+  const event = props.event;
+  console.log(event);
+  console.log(user);
+  const [showAlert, setShowAlert] = useState(false);
+  const handleDelete = () => {
+    console.log("delete from event card preview");
+    setShowAlert(false);
+    // deleteEvent(current._id);
+    // clearCurrent();
+    // clearUsers();
+    // history.push("/user");
+  };
+  const handleJoin = () => {
+    console.log("join from event card preview");
+  };
+  const handleUnjoin = () => {
+    console.log("unjoin from event card preview");
+  };
 
-    const [showAlert, setShowAlert] = useState(false);
-    const handleDelete = () => {
-        console.log("delete from event card preview");
-        setShowAlert(false);
-        // deleteEvent(current._id);
-        // clearCurrent();
-        // clearUsers();
-        // history.push("/user");
-    };
-    const handleJoin = () => { 
-        console.log("join from event card preview");
-    };
-    const handleUnjoin = () => { 
-        console.log("unjoin from event card preview");
-    };
+  // <DeleteButton setCurrent={setCurrent} setShowAlert={setShowAlert} />
 
-    // <DeleteButton setCurrent={setCurrent} setShowAlert={setShowAlert} />
-
-    if (showAlert) {
-        return (
-            // <Card style={{ width: "25rem" }}>
-            <Card>
-                <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
-                    <Alert.Heading>Are you sure you want to delete this event?</Alert.Heading>
-                    <Button className="btn-danger" onClick={handleDelete}>Yes</Button>
-                </Alert>
-            </Card>
-        )
-    }
-
+  if (showAlert) {
     return (
-        <Card>
-            <Card.Header style={{ background: "#343a40", color: "white" }}>
-                <Card.Title>preview {event.name.toUpperCase()}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{event.category}</Card.Subtitle>
-            </Card.Header>
-            <Card.Body>
-                <Card.Text style={{ textTransform: "capitalize" }}>
-                    Location: {event.location}<br />
-                    Start: {event.start}<br />
-                    End: {event.end}
-                </Card.Text>
-                <Card.Subtitle className="mb-2 text-muted">
-                    People Going: {event.attendingId.length} / {event.groupSize}
-                </Card.Subtitle>
-            </Card.Body>
-            <Card.Footer style={{ background: "#343a40", color: "white" }}>
-                {/* <Link onClick={() => props.setCurrent(event)} to="/view" className="card-link">View</Link> */}
-                <Link to="/view" className="card-link">View</Link>
-                
-                {
-                    user._id === event.user &&
-                    // {/* <Link className="card-link" to="/create" onClick={() => setCurrent(event)}>Edit</Link>
-                    <DeleteButton setShowAlert={setShowAlert} />
-                }
-
-                {
-                    user._id !== event.user && !event.attendingId.includes(user._id) &&
-                    <JoinButton handleJoin={handleJoin} />
-                }
-                {
-                    event.user !== user._id && event.attendingId.includes(user._id) &&
-                    <LeaveButton handleUnjoin={handleUnjoin} />
-                }
-            </Card.Footer>
-        </Card>
+      // <Card style={{ width: "25rem" }}>
+      <Card>
+        <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+          <Alert.Heading>
+            Are you sure you want to delete this event?
+          </Alert.Heading>
+          <Button className="btn-danger" onClick={handleDelete}>
+            Yes
+          </Button>
+        </Alert>
+      </Card>
     );
+  }
 
-}
+  return (
+    <Fragment>
+      {event.attendingId && (
+        <Card>
+          <Card.Header style={{ background: "#343a40", color: "white" }}>
+            <Card.Title>preview {event.name.toUpperCase()}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {event.category}
+            </Card.Subtitle>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text style={{ textTransform: "capitalize" }}>
+              Location: {event.location}
+              <br />
+              Start: {event.start}
+              <br />
+              End: {event.end}
+            </Card.Text>
+            <Card.Subtitle className="mb-2 text-muted">
+              People Going: {event.attendingId.length} / {event.groupSize}
+            </Card.Subtitle>
+          </Card.Body>
+          <Card.Footer style={{ background: "#343a40", color: "white" }}>
+            {/* <Link onClick={() => props.setCurrent(event)} to="/view" className="card-link">View</Link> */}
+            <Link to="/view" className="card-link">
+              View
+            </Link>
+
+            {user._id === event.user && (
+              // {/* <Link className="card-link" to="/create" onClick={() => setCurrent(event)}>Edit</Link>
+              <DeleteButton setShowAlert={setShowAlert} />
+            )}
+
+            {user._id !== event.user &&
+              !event.attendingId.includes(user._id) && (
+                <JoinButton handleJoin={handleJoin} />
+              )}
+            {event.user !== user._id &&
+              event.attendingId.includes(user._id) && (
+                <LeaveButton handleUnjoin={handleUnjoin} />
+              )}
+          </Card.Footer>
+        </Card>
+      )}
+    </Fragment>
+  );
+};
 
 export default EventCardPreview;
--- client/src/pages/SearchEvent.js
+++ client/src/pages/SearchEvent.js
@@ -7,69 +7,67 @@ import AuthContext from "../context/auth/authContext";
 import Button from "react-bootstrap/Button";
 import Loading from "../components/Loading";
 import history from "../utils/history";
-import Container from "react-bootstrap/Container"
-import CardDeck from "react-bootstrap/CardDeck"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-
+import Container from "react-bootstrap/Container";
+import CardDeck from "react-bootstrap/CardDeck";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 import EventCardPreview from "../components/events/EventCardPreview";
 
 const SearchEvent = () => {
-    useEffect(() => {
-        if (!user) {
-            authContext.loadUser();
-        }
-    });
-    const authContext = useContext(AuthContext);
-    const { user, isAuthenticated } = authContext;
-    const text = useRef("");
-    const eventContext = useContext(EventContext);
-    const {
-        getEvents,
-        clearUsers,
-        clearCurrent,
-        filterEvents,
-        clearFilter,
-        events,
-        filtered
-    } = eventContext;
-    console.log(events);
-
-    useEffect(() => {
-        getEvents();
-        clearUsers()
-        clearCurrent()
-        // eslint-disable-next-line
-    }, []);
-
-    const handleChange = e => {
-        if (text.current.value !== "") {
-            filterEvents(e.target.value);
-        } else {
-            clearFilter();
-        }
-    };
-    // const eventCategories = ["Movie","Concert","Food/Drink","Bar/Club","Gaming","Coding","Party","Conversation","Other"];
-
-    if (!events) {
-        return <Loading />;
+  useEffect(() => {
+    if (!user) {
+      authContext.loadUser();
     }
-    console.log(events);
+  });
+  const authContext = useContext(AuthContext);
+  const { user, isAuthenticated } = authContext;
+  const text = useRef("");
+  const eventContext = useContext(EventContext);
+  const {
+    getEvents,
+    clearUsers,
+    clearCurrent,
+    filterEvents,
+    clearFilter,
+    events,
+    filtered
+  } = eventContext;
+  console.log(events);
 
-    return (
+  useEffect(() => {
+    getEvents();
+    clearUsers();
+    clearCurrent();
+    // eslint-disable-next-line
+  }, []);
 
-        <Container>
-            {/* search bar - event name, category, time frame, distance (google api) */}
-            <Card style={{ width: "90%", margin: "0 auto" }}>
-                <Card.Body>
-                    <Card.Title>Event Search</Card.Title>
-                    <Form>
-                        <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control ref={text} onChange={handleChange} />
-                        </Form.Group>
-                        { /*<Form.Group controlId="exampleForm.ControlSelect2">
+  const handleChange = e => {
+    if (text.current.value !== "") {
+      filterEvents(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+  // const eventCategories = ["Movie","Concert","Food/Drink","Bar/Club","Gaming","Coding","Party","Conversation","Other"];
+
+  if (!events) {
+    return <Loading />;
+  }
+  console.log(events);
+
+  return (
+    <Container>
+      {/* search bar - event name, category, time frame, distance (google api) */}
+      <Card style={{ width: "90%", margin: "0 auto" }}>
+        <Card.Body>
+          <Card.Title>Event Search</Card.Title>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control ref={text} onChange={handleChange} />
+            </Form.Group>
+            {/*<Form.Group controlId="exampleForm.ControlSelect2">
               <Form.Label>Category</Form.Label>
               <Form.Control as="select" multiple>
                 {eventCategories.map((category, i) => (
@@ -82,37 +80,33 @@ const SearchEvent = () => {
               <Form.Control type="text" />
             </Form.Group>
             /* ADD TIME RANGE */}
-                        <Button className="float-right">Search</Button>
-                    </Form>
-
-                </Card.Body>
-            </Card>
-            <CardDeck>
-                <Row className="mt-5 mb-5">
-                    {events === null
-                        ? (
-                            <div>No Events Available</div>
-                        )
-                        : filtered !== null ? (
-                            filtered.map(event =>
-                                // <Col xs={12} sm={9} md={6} lg={4}>
-                                <EventItem key={event._id} event={event} />
-                                // <EventCardPreview key={event._id} event={event} />
-                                // </Col>
-                            )
-                        )
-                            : (
-                                events.map(event =>
-                                    // <Col xs={12} sm={9} md={6} lg={4}>
-                                    <EventItem key={event._id} event={event} />
-                                    // <EventCardPreview key={event._id} event={event} />
-                                    // </Col>
-                                )
-                            )}
-                </Row>
-            </CardDeck>
-        </Container>
-    );
+            <Button className="float-right">Search</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <CardDeck>
+        <Row className="mt-5 mb-5">
+          {events === null ? (
+            <div>No Events Available</div>
+          ) : filtered !== null ? (
+            filtered.map(event => (
+              <Col xs={12} sm={9} md={6} lg={4}>
+                {/* <EventItem key={event._id} event={event} /> */}
+                <EventCardPreview key={event._id} event={event} user={user} />
+              </Col>
+            ))
+          ) : (
+            events.map(event => (
+              <Col xs={12} sm={9} md={6} lg={4}>
+                {/* <EventItem key={event._id} event={event} /> */}
+                <EventCardPreview key={event._id} event={event} user={user} />
+              </Col>
+            ))
+          )}
+        </Row>
+      </CardDeck>
+    </Container>
+  );
 };
 
 export default SearchEvent;
