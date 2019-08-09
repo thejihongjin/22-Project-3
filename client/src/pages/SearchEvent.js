b0bbb5726b1cac880974d47266b704177b57da06
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-08-09 01:39:27 -0700

    moved Toast to separate component (not working yet), replaced EventItem with EventCardPreview component in SearchEvent.js page

--- client/src/pages/SearchEvent.js
+++ client/src/pages/SearchEvent.js
@@ -1,16 +1,12 @@
 import React, { useContext, useEffect, useRef, Fragment } from "react";
+import AuthContext from "../context/auth/authContext";
 import EventContext from "../context/event/eventContext";
-import EventItem from "../components/events/EventItem";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
-import AuthContext from "../context/auth/authContext";
+import CardColumns from "react-bootstrap/CardColumns";
 import Button from "react-bootstrap/Button";
 import Loading from "../components/Loading";
-import history from "../utils/history";
-import Container from "react-bootstrap/Container"
-import CardDeck from "react-bootstrap/CardDeck"
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
+// import history from "../utils/history";
 
 
 import EventCardPreview from "../components/events/EventCardPreview";
@@ -55,13 +51,11 @@ const SearchEvent = () => {
     if (!events) {
         return <Loading />;
     }
-    console.log(events);
-
     return (
 
-        <Container>
+        <Fragment>
             {/* search bar - event name, category, time frame, distance (google api) */}
-            <Card style={{ width: "90%", margin: "0 auto" }}>
+            <Card>
                 <Card.Body>
                     <Card.Title>Event Search</Card.Title>
                     <Form>
@@ -84,34 +78,23 @@ const SearchEvent = () => {
             /* ADD TIME RANGE */}
                         <Button className="float-right">Search</Button>
                     </Form>
-
                 </Card.Body>
             </Card>
-            <CardDeck>
-                <Row className="mt-5 mb-5">
-                    {events === null
-                        ? (
-                            <div>No Events Available</div>
-                        )
-                        : filtered !== null ? (
-                            filtered.map(event =>
-                                // <Col xs={12} sm={9} md={6} lg={4}>
-                                <EventItem key={event._id} event={event} />
-                                // <EventCardPreview key={event._id} event={event} />
-                                // </Col>
-                            )
-                        )
-                            : (
-                                events.map(event =>
-                                    // <Col xs={12} sm={9} md={6} lg={4}>
-                                    <EventItem key={event._id} event={event} />
-                                    // <EventCardPreview key={event._id} event={event} />
-                                    // </Col>
-                                )
-                            )}
-                </Row>
-            </CardDeck>
-        </Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Results</Card.Title>
+                    <CardColumns>
+                        {
+                            events === null
+                                ? <div>No Events Available</div>
+                                : filtered !== null
+                                    ? filtered.map(event => <EventCardPreview key={event._id} event={event} user={user} />)
+                                    : events.map(event => <EventCardPreview key={event._id} event={event} user={user} />)
+                        }
+                    </CardColumns>
+                </Card.Body>
+            </Card>
+        </Fragment>
     );
 };
 
